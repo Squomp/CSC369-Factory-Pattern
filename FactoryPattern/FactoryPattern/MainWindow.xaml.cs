1a6963562bb97ae763f486f10aec332203bd6010
Mitchell Hartweg <mhartweg@student.neumont.edu>
2018-04-12 14:43:03 -0600

    fileIO finished but not tested

--- FactoryPattern/FactoryPattern/MainWindow.xaml.cs
+++ FactoryPattern/FactoryPattern/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +25,35 @@ namespace FactoryPattern
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public static void WriteAndRunHTML(string componentCode)
+        {
+            string path = "code.html";
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                w.Write(componentCode);
+            }
+            System.Diagnostics.Process.Start(path);
+        }
+
+        public static void WriteAndRunXAML(string componentCode)
+        {
+            string path = "code.xaml";
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                w.Write(componentCode);
+            }
+
+            UIElement root;
+            using (FileStream s = new FileStream(path, FileMode.Open))
+            {
+                root = (UIElement)XamlReader.Load(s);
+            }
+
+            ChildWindow child = new ChildWindow();
+            child.Preview = (Grid)root;
+            child.ShowDialog();
         }
     }
 }
